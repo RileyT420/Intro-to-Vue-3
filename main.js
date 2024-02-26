60470366487de029a7a23cb704ba9d64389af3cf
Riley Tate <124rtate@frhsd.com>
2024-02-26 10:34:46 -0500

    Lesson 6

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 1,
             product: 'Socks',
             description: 'They are socks',
             image: './assets/images/socks_green.jpg',
@@ -9,10 +10,18 @@ const app = Vue.createApp({
             onSale: true,
             details: ['50% Cotton', '30% Wool', '20% polester'],
             variants: [
-                { id: 2234, color: 'green'},
-                { id: 2235, color: 'blue'},
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
             ],
             sizes: ['xsmall', 'small', 'medium', 'large', 'xlarge']
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1;
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
         }
     }
 })
